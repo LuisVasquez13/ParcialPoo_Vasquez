5c00e84f8d28eb919c7f1fc600206d3831624d94
LuisVasquez13 <luis.vasquez14@utp.ac.pa>
2025-10-15 11:26:06 -0500

    Añadida clase Calificacion

--- Clases/Calificacion.cs
+++ Clases/Calificacion.cs
@@ -6,7 +6,45 @@ using System.Threading.Tasks;
 
 namespace ParcialPoo_Vásquez.Clases
 {
-    internal class Calificacion
+    // Clase Calificacion que implementa la interfaz IMostrable
+    public class Calificacion : IMostrable
     {
+        private Estudiante estudiante;
+        private Materia materia;
+        private double nota;
+
+        public Estudiante Estudiante // Propiedad Estudiante con validación
+        {
+            get => estudiante;
+            // Validar que el estudiante no sea nulo
+            set => estudiante = value ?? throw new ArgumentException("El estudiante no puede ser nulo");
+        }
+
+        public Materia Materia // Propiedad Materia con validación
+        {
+            get => materia;
+            // Validar que la materia no sea nula
+            set => materia = value ?? throw new ArgumentException("La materia no puede ser nula");
+        }
+
+        public double Nota // Propiedad Nota con validación
+        {
+            get => nota;
+            set
+            {
+                if (value < 0 || value > 100) // Validación de nota entre 0 y 100
+                    throw new ArgumentException("La nota debe estar entre 0 y 100");
+                nota = value;
+            }
+        }
+
+        // Constructor
+        public Calificacion(Estudiante estudiante, Materia materia, double nota)
+        {
+            Estudiante = estudiante;
+            Materia = materia;
+            Nota = nota;
+        }
+
     }
 }
