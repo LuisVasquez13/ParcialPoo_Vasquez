c88d36f46a1cbb993e95e513daaaea642e01fb73
LuisVasquez13 <luis.vasquez14@utp.ac.pa>
2025-10-15 11:09:45 -0500

    Añadida parte de la clase Estudiante

---
+++ Clases/Estudiante.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ParcialPoo_Vásquez.Clases;
+
+namespace ParcialPoo_Vásquez.Clases
+{
+    public class Estudiante : IMostrable
+    {
+        private string nombre;
+        private string id;
+        private string carrera;
+        private List<Calificacion> calificaciones = new List<Calificacion>();
+
+        public string Nombre
+        {
+            get => nombre;
+            set => nombre = value ?? throw new ArgumentException("El nombre no puede ser nulo");
+        }
+
+        public string Id
+        {
+            get => id;
+            set => id = value ?? throw new ArgumentException("El ID no puede ser nulo");
+        }
+
+        public string Carrera
+        {
+            get => carrera;
+            set => carrera = value ?? throw new ArgumentException("La carrera no puede ser nula");
+        }
+
+    }
+}
