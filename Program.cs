caf743aef0ad346fdd245c47314af0548cc3315f
LuisVasquez13 <luis.vasquez14@utp.ac.pa>
2025-10-15 11:41:09 -0500

    Añadido codigo al Main

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Programa principal
+public class Program
+{
+    public static void Main()
+    {
+        // --- Crear materias ---
+        var m1 = new Materia("Programación", "INF101", 3, 30, 25);
+        var m2 = new Materia("Base de Datos", "INF202", 4, 30, 28);
+        var m3 = new Materia("Matemáticas", "MAT105", 2, 25, 20);
+
+    }
+}
