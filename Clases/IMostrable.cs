22cb62ea38d3dd0e896fd9636ab1c914906f29eb
LuisVasquez13 <luis.vasquez14@utp.ac.pa>
2025-10-15 10:54:46 -0500

    Agregar archivos de proyecto.

---
+++ Clases/IMostrable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParcialPoo_Vásquez.Clases
+{
+    public interface IMostrable // Interfaz para mostrar datos
+    {
+        void MostrarDatos(); // Método para mostrar datos
+    }
+
+}
